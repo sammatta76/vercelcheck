3ec940706cb0360954d99742bec154e6e626f71e
sam <samermatta2003@gmail.com>
2024-01-24 13:32:11 -0600

    first commit

---
+++ main.py
@@ -0,0 +1,30 @@
+
+import fastapi
+from fastapi.responses import RedirectResponse
+from mangum import Mangum
+app = fastapi.FastAPI()
+handler = Mangum(app)
+
+
+info = dict()
+
+
+@app.get("/")
+async def read_root():
+    return {"message": "Hello, FastAPI!"}
+
+@app.get("/expired")
+async def exp():
+    return {"message": "subscription expired"}
+
+
+@app.get("/fetch-url/{name}/")
+async def fetch_url(name : str):
+    name = name.lower()
+    if name == "teki":
+        return RedirectResponse(url="/expired")
+    url_path = info.get(name, "Hello")
+    google_url = "https://www.google.com"
+    print("redirected")
+    return RedirectResponse(url=google_url)
+    # return {"message": f"Fetching data from URL: {url_path}"}
